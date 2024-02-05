f49504f4ab3b48f3fe5dd0bfe41aa8c32d79ff19
Pablo Lopez Sardi <lopezsardip@gmail.com>
2024-02-04 21:33:07 -0300

    ejercicio termindo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Saludo from './Saludo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Saludo particularizacion='My Friend'></Saludo>
     </>
   )
 }
---
+++ src/Saludo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Saludo = ({particularizacion}) => {
+    return (
+        <>
+            <h1>Hello {particularizacion}</h1>
+        </>
+    );
+};
+
+export default Saludo;
